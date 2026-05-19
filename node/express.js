0b210eb0afd3cd23fa971f24cfc0ed76079cd159
Amit Kumar <amit.kumar@getvymo.com>
2026-05-20 01:05:23 +0530

    create Routes

--- node/express.js
+++ node/express.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next)=>{
-    req.user = "Guest";
-    next();
-})
+app.get('/orders', (req, res) => {
+  res.send('Here is the list of all orders.');
+});
 
-app.get('/welcome', (req, res)=>{
-    res.send(`<h1>Welcome, ${req.user} !</h1>`);
-})
+app.post('/orders', (req, res) => {
+  res.send('A new order has been created.');
+});
 
-app.listen(3000);
+app.get('/users', (req, res) => {
+  res.send('Here is the list of all users.');
+});
+
+app.post('/users', (req, res) => {
+  res.send('A new user has been added.');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
